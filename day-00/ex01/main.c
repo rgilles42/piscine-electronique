5b9940c6a8400f7e4199ef0d24df5ae38d905a98
Raphael GILLES <rgilles@student.42.fr>
2023-03-13 12:49:31 +0100

    finished ex01

--- day-00/ex01/main.c
+++ day-00/ex01/main.c
@@ -1,1 +1,7 @@
-int main(void){}
+#include <avr/io.h>
+
+int main(void){
+    DDRB = 0xFF;        // Configure PORTB as output
+    PORTB = 0b00000001; // Turn ON the LED connected to pin 0 of PORTB
+    return 0;
+}
