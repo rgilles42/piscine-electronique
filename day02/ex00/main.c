0a3d3f3f279a0ce568ec0b610fd28120594c85ab
Raphael GILLES <rgilles@student.42.fr>
2023-03-15 14:12:13 +0100

    fixed interrupt logic for bouncing handling

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,8 +3,12 @@
 #include <avr/interrupt.h>
 
 ISR(INT0_vect) {
-    _delay_ms(1);
-    PORTB ^= _BV(PB0);                      // Flip PB0.
+    static uint8_t falling_edge = 1;
+    if (falling_edge)
+        PORTB ^= _BV(PB0);                  // Flip PB0.
+    _delay_ms(1);                           // Ignore bounce-back.
+    falling_edge ^= 1;                      // Next interrupt will be for rising edge.
+    EIFR |= _BV(INT0);                      // Clear the interrupt flag for any bouncing that happened during the pause.
 }
 
 int main(void) {
@@ -12,7 +16,7 @@ int main(void) {
     DDRB = _BV(PB0);                        // Set PB0 as output.
     PORTB = 0x00;                           // Clear PORTB.
 
-    EICRA = _BV(ISC01);                     // Set External Interrupt Control to Falling Edge mode for INT0.
+    EICRA = _BV(ISC00);                     // Set External Interrupt Control to React To Any Logical Change mode for INT0.
     EIMSK = _BV(INT0);                      // Declare INT0 in the External Interrupt Mask Register
     sei();
     while(1);
